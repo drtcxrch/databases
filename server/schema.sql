ea558e25412c79a08bd357d5545d12a5d45d193e
Calvin <calvinminhla@gmail.com>
2021-01-12 16:58:36 -0800

    Working on creating multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,13 +2,30 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE users (
+  id int,
+  username varchar(255),
+  friends set,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE rooms (
+  id int,
+  room_name varchar(255),
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id int,
+  user_message varchar(255),
+  id_users int,
+  id_rooms int,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id_users) REFERENCES users(id),
+  FOREIGN KEY (id_rooms) REFERENCES rooms(id)
 );
 
 /* Create other tables and define schemas for them here! */
-
-
 
 
 /*  Execute this file from the command line by typing:
